6010b584b482713e5226ade279accff934db47de
christoph47112 <55946870+christoph47112@users.noreply.github.com>
2024-12-04 02:08:47 +0100

    Update sales_data_web_app_enhanced.py

--- sales_data_web_app_enhanced.py
+++ sales_data_web_app_enhanced.py
@@ -81,12 +81,11 @@ elif page == text["instructions"]:
     # Anleitung
     st.markdown(text["instructions_text"])
 
-    # Button mit sofortiger Reaktion
+    # Button mit sofortiger Funktionalität
     if st.button(
         "Nur Anleitung anzeigen" if st.session_state["show_module"] else "Modul benutzen und Anleitung anzeigen"
     ):
         st.session_state["show_module"] = not st.session_state["show_module"]
-        st.experimental_rerun()  # Sofortige Neurenderung nach Zustand-Update
 
     # Beispieldatei Download
     example_data = {
